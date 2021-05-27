e6235cb9143a7b560f38d1ffd22cca319a35d500
Ahmet İkbal Adlığ <a-ikbal@hotmail.com>
2021-05-27 23:32:14 +0300

    Highscores.java edited

--- src/application/HighScores.java
+++ src/application/HighScores.java
@@ -1,58 +1,66 @@
 package application;
 
-
-import java.io.File; 
-import java.io.IOException; 
+import java.io.File;
+import java.io.IOException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class HighScores {
-	
-	public static void high(int level,int newScore) throws IOException {
-		String file = "highscores/high"+level+".txt";
+
+	public static void high(int level, int newScore) throws IOException {
+
+		String file = "highscores/high" + level + ".txt";
 		File lvlfile = new File(file);
-		if (lvlfile.createNewFile()) {
-	        System.out.println("File created: " + lvlfile.getName());
-	      } 
-		else {
-	        System.out.println("File already exists.");
-	    }
+
+		if (lvlfile.createNewFile())
+			System.out.println("File created: " + lvlfile.getName());
+		else
+			System.out.println("File already exists.");
+
 		Scanner sc = new Scanner(file);
 		PrintWriter writer = new PrintWriter(file);
-		if(!sc.hasNext()) {
+
+		if (!sc.hasNext()) {
+
 			writer.print(String.valueOf(newScore));
-			
-		}
-		else {
+
+		} else {
+
 			String old = sc.next();
 			int oldHigh = Integer.valueOf(old);
-			if(oldHigh<newScore) {
+
+			if (oldHigh < newScore) {
+
 				old.replaceAll(old, String.valueOf(newScore));
 				writer.print(old);
-				
-			}
-			else {
-				
-			}
-		}
-		
 
-  }
+			} else {
+
+			}
+
+		}
+
+	}
+
 	public static int getHigh(int level) {
-		File file = new File("highscores/high"+level+".txt");
-		Scanner sc = new Scanner("highscores/high"+level+".txt");
-		if(file.exists()) {
-			if(sc.hasNext()) {
+
+		File file = new File("highscores/high" + level + ".txt");
+		Scanner sc = new Scanner("highscores/high" + level + ".txt");
+
+		if (file.exists()) {
+
+			if (sc.hasNext())
 				return sc.nextInt();
-			}
-			else {
+			else
+				return 0;
+
+		} else {
+
 			return 0;
-			}
+
 		}
-		else {
-			return 0;
-		}
+
 	}
 }
